b9d487c54fe25d0e72a36efdc52be068de80a140
Neo <neoscript@gmail.com>
2020-01-15 17:12:58 +0800

    附件上传成功

--- src/ant-design-field/UploadField.tsx
+++ src/ant-design-field/UploadField.tsx
@@ -3,19 +3,23 @@ import { Upload, Button } from 'antd';
 import { FieldProps } from './FieldProps';
 import { AbstractField } from './AbstractField';
 import { UploadProps } from 'antd/lib/upload';
-import { GetFieldDecoratorOptions } from 'antd/lib/form/Form';
 
 export class UploadField extends AbstractField<UploadProps & FieldProps> {
   getField() {
     return (
-      <Upload {...this.getInputProps()}>
+      <UploadWrap {...this.getInputProps()}>
         <Button icon="upload">选择文件</Button>
-      </Upload>
+      </UploadWrap>
     );
   }
-  get defaultDecorator(): GetFieldDecoratorOptions {
-    return {
-      valuePropName: 'fileList',
-    };
+}
+interface UploadWrapProps extends UploadProps {
+  value?: any;
+}
+class UploadWrap extends React.Component<UploadWrapProps> {
+  render() {
+    const { value, ...uploadProps } = this.props;
+    const fileList = value && (Array.isArray(value) ? value : [value]);
+    return <Upload {...uploadProps} fileList={fileList} />;
   }
 }
